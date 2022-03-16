fdf5e56ddcec7db1af9f9c010c7bfa7200d2ff19
niiyazobug <niiyaz5555@gmail.com>
2022-03-16 15:46:02 +0600

    Load dynamic data, API call useEffect integrate state

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-// Concept Recap, JSX, components, props, display array of objects
+//------ Concept Recap, JSX, components, props, display array of objects --------
 //  function App() {
 //   const products = [
 //     { name: 'Laptopoo', price: '153000' },
@@ -35,39 +35,83 @@ import { useState } from 'react';
 // } 
 
 
-// State, component state hook and set state method
+
+//------ State, component state hook and set state method--------
+// function App() {
+
+//   return (
+//     <div className="App">
+//       <Counter></Counter>
+//     </div>
+//   );
+// }
+
+// function Counter() {
+//   /* useState() function component er moddhe state variable ke rakhte day */
+//   const [count, setCount] = useState(33)
+
+//   // handler
+//   // system 1 
+//   // const increaseCount = () => {
+//   //   const newCount = count + 1
+//   //   setCount(newCount)
+//   // }
+
+//   // system 2
+//   const increaseCount = () => setCount(count + 1)
+//   const decreaseCount = () => setCount(count - 1)
+
+
+//   return (
+//     <div>
+//       <h1>Count: {count}</h1>
+//       {/* normal js a fucntion ke onclick er moddhe call korte hoi, but aykhane onClick a just function name ta likhei hobe call kora lagbe na */}
+//       <button onClick={increaseCount}> Increase </button>
+//       <br />
+//       <button onClick={decreaseCount}> Decrease </button>
+//     </div>
+//   )
+// }
+
+
+
+// --------- Load dynamic data, API call useEffect integrate state ----------
 function App() {
 
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
-function Counter() {
-  /* useState() function component er moddhe state variable ke rakhte day */
-  const [count, setCount] = useState(33)
+function ExternalUsers() {
+  const [users, setUsers] = useState([])
+  useEffect(() => {
 
-  // handler
-  // system 1 
-  // const increaseCount = () => {
-  //   const newCount = count + 1
-  //   setCount(newCount)
-  // }
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
 
-  // system 2
-  const increaseCount = () => setCount(count + 1)
-  const decreaseCount = () => setCount(count - 1)
-  
+  }, [])
 
   return (
     <div>
-      <h1>Count: {count}</h1>
-      {/* normal js a fucntion ke onclick er moddhe call korte hoi, but aykhane onClick a just function name ta likhei hobe call kora lagbe na */}
-      <button onClick={increaseCount}> Increase </button>
-      <br />
-      <button onClick={decreaseCount}> Decrease </button>
+      <h2> External Users </h2>
+      <p>{users.length}</p>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{border:'2px solid orange', borderRadius:'10px', margin:'20px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>{props.email}</p>
     </div>
   )
 }
